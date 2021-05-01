af2f90ceb8fed981e36570f36a00b8888b883a19
Takehiko TERADA <takehiko.terada@gmail.com>
2021-05-01 20:02:47 +0900

    Convert strands data to Wavefront OBJ format.

---
+++ convert2Obj.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+import struct
+
+
+def loadObjFile(filename):
+	print("# ", filename)
+	vertexNumber = 1
+	prevVertNum = 1
+	infile = open(filename, 'rb')
+	data = infile.read(4)
+	if data:
+		numStride = struct.unpack('<i', data)[0]
+		#print(numStride)
+	for i in range(numStride):
+		data = infile.read(4)
+		if data:
+			numVert = struct.unpack('<i', data)[0]
+			#print(numVert)
+			startVertNum = vertexNumber
+			for j in range(numVert):
+				d1 = infile.read(4)
+				d2 = infile.read(4)
+				d3 = infile.read(4)
+				v1 = struct.unpack('<f', d1)[0]
+				v2 = struct.unpack('<f', d2)[0]
+				v3 = struct.unpack('<f', d3)[0]
+				print("v ", v1, v2, v3)
+				vertexNumber += 1
+			print("l ", end="")
+			for j in range(vertexNumber - startVertNum):
+				print(j + prevVertNum, " ", end="")
+			print("")
+			prevVertNum = vertexNumber
+
+
+def main(filename):
+	loadObjFile(filename)
+
+if __name__ == "__main__":
+	args = sys.argv
+	filename = ''
+	if 1 == len(args):
+		print('Need a file')
+		quit()
+	else:
+		filename = args[1]
+	main(filename)
